19aa4c601899fa030b107d96eaa48c17adfa6e36
Giles Hinchcliff <gileshinchcliff@yahoo.com>
2016-09-29 15:52:22 +0100

    updated to simplify

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,31 +4,4 @@ set -m
 
 # start in background
 freshclam -d &
-clamd &
-
-# recognize PIDs
-pidlist=`jobs -p`
-
-# initialize latest result var
-latest_exit=0
-
-# define shutdown helper
-function shutdown() {
-    trap "" SUBS
-
-    for single in $pidlist; do
-        if ! kill -0 $pidlist 2>/dev/null; then
-            wait $pidlist
-            exitcode=$?
-        fi
-    done
-
-    kill $pidlist 2>/dev/null
-}
-
-# run shutdown
-trap terminate SUBS
-wait
-
-# return received result
-exit $latest_exit
+clamd 

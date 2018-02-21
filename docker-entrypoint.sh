83f20bab93b9da0b409fac370aedf735027b3f4f
Gerard Krupa <GerardJohn.Krupa@homeoffice.gsi.gov.uk>
2018-02-21 17:34:47 +0000

    #27 Documented configuration by environment variable is not implemented

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,31 @@
 # bootstrap clam av service and clam av database updater
 set -m
 
+function process_file() {
+    if [[ ! -z "$1" ]]; then
+        local SETTING_LIST=$(echo "$1" | tr ',' '\n' | grep "^[A-Za-z][A-Za-z]*=.*$")
+        local SETTING
+        
+        for SETTING in ${SETTING_LIST}; do
+            # Remove any existing copies of this setting.  We do this here so that
+            # settings with multiple values (e.g. ExtraDatabase) can still be added
+            # multiple times below
+            local KEY=${SETTING%%=*}
+            sed -i $2 -e "/^${KEY} /d"
+        done
+
+        for SETTING in ${SETTING_LIST}; do
+            # Split on first '='
+            local KEY=${SETTING%%=*}
+            local VALUE=${SETTING#*=}
+            echo "${KEY} ${VALUE}" >> "$2"
+        done
+    fi
+}
+
+process_file "${CLAMD_SETTINGS_CSV}" /usr/local/etc/clamd.conf
+process_file "${FRESHCLAM_SETTINGS_CSV}" /usr/local/etc/freshclam.conf
+
 # start in background
 freshclam -d &
 clamd 
